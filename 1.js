67b17e068d1b03c05ef5fe129bf02c15c30dc1c9
Will Smith <will.smith@simplisafe.com>
2019-12-03 22:20:30 -0500

    #1 initial commit

---
+++ 1.js
@@ -0,0 +1,9 @@
+// Day 1: The Tyranny of the Rocket Equation
+const fs = require('fs');
+
+let modules = fs.readFileSync('1_input.txt', 'utf-8').split('\n');
+console.log(modules);
+
+for (let module of modules) {
+  // calculate fuel req
+}
