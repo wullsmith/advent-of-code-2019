9d7799791a0185570e8109e82450ad7bd19a6212
wullsmith <wqs314@gmail.com>
2019-12-10 23:01:17 -0500

    day 9

--- 9.js
+++ 9.js
@@ -36,32 +36,38 @@ function runIntcode(program, inputs, ipStart=0) {
     let m2 = instruction.charAt(1);
     let m3 = instruction.charAt(0); // unused
 
-    // get parameters
+    // get parameters (addresses of operands)
     let p1, p2, p3;
 
-    if (m1 === '0') p1 = program[program[i+1]];
-    else if (m1 === '1') p1 = program[i+1];
-    else if (m1 === '2') p1 = program[program[i+1] + rb];
+    if (m1 === '0') p1 = program[i+1];
+    else if (m1 === '1') p1 = i+1;
+    else if (m1 === '2') p1 = program[i+1] + rb;
 
-    if (m2 === '0') p2 = program[program[i+2]];
-    else if (m2 === '1') p2 = program[i+2];
-    else if (m2 === '2') p2 = program[program[i+2] + rb];
+    if (m2 === '0') p2 = program[i+2];
+    else if (m2 === '1') p2 = i+2;
+    else if (m2 === '2') p2 = program[i+2] + rb;
 
-    // """memory"""
-    if (p1 === undefined) p1 = 0;
-    if (p2 === undefined) p2 = 0;
+    if (m3 === '0') p3 = program[i+3];
+    else if (m3 === '1') {
+      debug('Error: output address set to immediate mode');
+      return;
+    }
+    else if (m3 === '2') p3 = program[i+3] + rb;
 
-    p3 = program[i+3]; // p3 always an address
+    // "memory"
+    if (program[p1] === undefined) program[p1] = 0;
+    if (program[p2] === undefined) program[p2] = 0;
 
+    // run instruction
     switch (opcode) {
       case 1: // addition
-        debug(`\t${p1} + ${p2} => [${p3}]`);
-        program[p3] = p1 + p2;
+        debug(`\t${program[p1]} + ${program[p2]} => [${p3}]`);
+        program[p3] = program[p1] + program[p2];
         i += 4;
         break;
       case 2: // multiplication
-        debug(`\t${p1} * ${p2} => [${p3}]`);
-        program[p3] = p1 * p2;
+        debug(`\t${program[p1]} * ${program[p2]} => [${p3}]`);
+        program[p3] = program[p1] * program[p2];
         i += 4;
         break;
       case 3: // input
@@ -74,42 +80,42 @@ function runIntcode(program, inputs, ipStart=0) {
             outputs,
           };
         }
-        let n = inputs.shift();
+        let n = parseInt(inputs.shift(), 10);
         program[p1] = n;
         debug(`\tinput: ${n} => [${p1}]`);
         i += 2;
         break;
       case 4: // output
-        program[0] = p1;
-        outputs.push(p1);
-        debug(`\toutput ${p1}`);
+        program[0] = program[p1];
+        outputs.push(program[p1]);
+        debug(`\toutput ${program[p1]}`);
         i += 2;
         break;
       case 5: // jump-if-true
-        debug(`\t${p1} != 0 ? j ${p2}`);
-        if (p1 !== 0) i = p2;
+        debug(`\t[${p1}]${program[p1]} != 0 ? j ${program[p2]}`);
+        if (program[p1] !== 0) i = program[p2];
         else i += 3;
         break;
       case 6: // jump-if-false
-        debug(`\t${p1} == 0 ? j ${p2}`);
-        if (p1 === 0) i = p2;
+        debug(`\t[${p1}]${program[p1]} == 0 ? j ${program[p2]}`);
+        if (program[p1] === 0) i = program[p2];
         else i += 3;
         break;
       case 7: // less than
-        debug(`\t${p1} < ${p2} ? 1 => [${p3}]`);
-        if (p1 < p2) program[p3] = 1;
+        debug(`\t[${p1}]${program[p1]} < [${p2}]${program[p2]} ? 1 => [${p3}]`);
+        if (program[p1] < program[p2]) program[p3] = 1;
         else program[p3] = 0;
         i += 4;
         break;
       case 8: // equals
-        debug(`\t${p1} == ${p2} ? 1 => [${p3}]`);
-        if (p1 === p2) program[p3] = 1;
+        debug(`\t[${p1}]${program[p1]} == [${p2}]${program[p2]} ? 1 => [${p3}]`);
+        if (program[p1] === program[p2]) program[p3] = 1;
         else program[p3] = 0;
         i += 4;
         break;
       case 9: // relative base offset
-        debug(`\trb: ${rb} + ${p1}`);
-        rb += p1;
+        debug(`\trb: ${rb} + ${program[p1]}`);
+        rb += program[p1];
         i += 2;
         break;
       case 99: // end
@@ -136,9 +142,9 @@ function runIntcode(program, inputs, ipStart=0) {
 
 // ----------------------------------------------------------------------------
 
+const DEBUG = false;
 const intcode = fs.readFileSync(process.argv[2], 'utf-8').split(',').map(s => parseInt(s, 10));
-const input = process.argv[3];
+const input = parseInt(process.argv[3], 10);
 
-const DEBUG = true;
 let res = runIntcode(intcode, [input]);
 fs.writeFileSync('9.log', res.program.toString().split(',').join('\n'));
